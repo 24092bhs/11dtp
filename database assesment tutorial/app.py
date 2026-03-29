edde7c5ecc6f7a3109e9333f76afc1dbdf3af0e4
24092bhs <24092@burnside.school.nz>
2026-03-30 09:53:56 +1300

    printed the data nicely

--- database assesment tutorial/app.py
+++ database assesment tutorial/app.py
@@ -7,12 +7,17 @@ DATABASE = "fighter.db"
 
 #functions
 def print_all_aircraft():
+    '''print all aircraft nicely'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     sql = "SELECT * FROM fighter;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print(results)
+    #loop through all results first
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<5}{fighter[3]:<4}{fighter[4]:<4}{fighter[5]:<4}{fighter[6]:<4}")
+    #loop finished here
     db.close()
 
 #main code
+print_all_aircraft()
