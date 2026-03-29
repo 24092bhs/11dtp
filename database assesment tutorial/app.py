06301e0233285d9ed17944971236a2e3ea0f3f0b
24092bhs <24092@burnside.school.nz>
2026-03-30 09:38:49 +1300

    followed the tutorial and did the basic structure

--- database assesment tutorial/app.py
+++ database assesment tutorial/app.py
@@ -1,1 +1,10 @@
-print('Hello, World')
+import sqlite3
+
+db = sqlite3.connect("fighter.db")
+cursor = db.cursor()
+sql = "SELECT * FROM fighter;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+
+db.close()
