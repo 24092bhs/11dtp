e1ac889f95226815cbedc97b65c0ef9ee63b0e93
24092bhs <24092@burnside.school.nz>
2026-03-31 09:48:01 +1300

    followed the main menu for the tutorial

--- database assesment tutorial/app.py
+++ database assesment tutorial/app.py
@@ -14,10 +14,18 @@ def print_all_aircraft():
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through all results first
+    print("Name                         Speed   Max_G Climb Range Payload")
     for fighter in results:
-        print(f"{fighter[1]:<30}{fighter[2]:<5}{fighter[3]:<4}{fighter[4]:<4}{fighter[5]:<4}{fighter[6]:<4}")
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
     #loop finished here
     db.close()
 
 #main code
-print_all_aircraft()
+while True:
+    user_input = input("\nWhat would you like to do? \n1. Print all aircraft \n2. Exit the program \n")
+    if user_input == "1":
+        print_all_aircraft()
+    elif user_input == '2':
+        break
+    else:
+        print('That was not an option!')
