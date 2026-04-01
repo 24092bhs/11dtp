d109635e58e236274a23b531dc26c493b99d3c88
24092bhs <24092@burnside.school.nz>
2026-04-01 13:49:26 +1300

    formatted the input statement thing

--- database assesment tutorial/app.py
+++ database assesment tutorial/app.py
@@ -1,12 +1,12 @@
-#24092 - airplane database application
-#imports
+# 24092 - airplane database application
+# imports
 import sqlite3
 
-#constants and variables
+# constants and variables
 DATABASE = "fighter.db"
 
-#functions
-#printing all aircraft
+# functions
+# printing all aircraft
 def print_all_aircraft():
     '''print all aircraft nicely'''
     db = sqlite3.connect(DATABASE)
@@ -14,14 +14,14 @@ def print_all_aircraft():
     print_aircraft = "SELECT * FROM fighter;"
     cursor.execute(print_aircraft)
     results = cursor.fetchall()
-    #loop through all results first
+    # loop through all results first
     print("Name                         Speed   Max_G Climb Range Payload")
     for fighter in results:
         print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
-    #loop finished here
+    # loop finished here
     db.close()
 
-#print all aircraft, but sort by speed
+# print all aircraft, but sort by speed
 def sort_by_speed():
     '''sort all aircraft nicely'''
     db = sqlite3.connect(DATABASE)
@@ -29,14 +29,14 @@ def sort_by_speed():
     sort_with_speed = "SELECT * FROM fighter ORDER BY speed DESC;"
     cursor.execute(sort_with_speed)
     results = cursor.fetchall()
-    #loop through all results first
+    # loop through all results first
     print("Name                         Speed   Max_G Climb Range Payload")
     for fighter in results:
         print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
-    #loop finished here
+    # loop finished here
     db.close()
 
- #print all aircraft, but sort by climb rate   
+# print all aircraft, but sort by climb rate   
 def sort_by_climbrate():
     '''sort all aircraft nicely'''
     db = sqlite3.connect(DATABASE)
@@ -44,14 +44,14 @@ def sort_by_climbrate():
     sort_with_climbrate = "SELECT * FROM fighter ORDER BY climb_rate DESC;"
     cursor.execute(sort_with_climbrate)
     results = cursor.fetchall()
-    #loop through all results first
+    # loop through all results first
     print("Name                         Speed   Max_G Climb Range Payload")
     for fighter in results:
         print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
-    #loop finished here
+    # loop finished here
     db.close()
 
-#print all aircraft, but sort by range
+# print all aircraft, but sort by range
 def sort_by_range():
     '''sort all aircraft nicely'''
     db = sqlite3.connect(DATABASE)
@@ -59,14 +59,14 @@ def sort_by_range():
     sort_with_range = "SELECT * FROM fighter ORDER BY range DESC;"
     cursor.execute(sort_with_range)
     results = cursor.fetchall()
-    #loop through all results first
+    # loop through all results first
     print("Name                         Speed   Max_G Climb Range Payload")
     for fighter in results:
         print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
-    #loop finished here
+    # loop finished here
     db.close()
 
-#print all range, but sort by payload
+# print all range, but sort by payload
 def sort_by_payload():
     '''sort all aircraft nicely'''
     db = sqlite3.connect(DATABASE)
@@ -74,16 +74,25 @@ def sort_by_payload():
     sort_with_payload = "SELECT * FROM fighter ORDER BY payload DESC;"
     cursor.execute(sort_with_payload)
     results = cursor.fetchall()
-    #loop through all results first
+    # loop through all results first
     print("Name                         Speed   Max_G Climb Range Payload")
     for fighter in results:
         print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
-    #loop finished here
+    # loop finished here
     db.close()
 
-#main code
+# main code
 while True:
-    user_input = input("\nWhat would you like to do? \n1. Print all aircraft \n2. Sort all aircraft by speed \n3. Sort all aircraft by climb rate \n4. Sort all aircraft by range \n5. Sort all aircraft by payload \n6. Exit the program\n")
+    user_input = input(
+    '''What would you like to do?
+    1. Print all aircraft
+    2. Sort all aircraft by speed
+    3. Sort all aircraft by climb rate
+    4. Sort all aircraft by range
+    5. Sort all aircraft by payload
+    6. Exit the program
+
+    ''')
     if user_input == "1":
         print_all_aircraft()
     elif user_input == '2':
