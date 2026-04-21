02a7beb9bafb02b113f32ac38d9259ac95d1295d
24092bhs <24092@burnside.school.nz>
2026-04-21 14:26:24 +1200

    wrote out all of the sql queries required

--- theatresports database folder/theatresports_code.py
+++ theatresports database folder/theatresports_code.py
@@ -1,7 +1,30 @@
 # 24092 - theatresports database application
 
-#imports
+# imports
 import sqlite3
 
-#constants and variables
+# constants and variables
 DATABASE = "theatresports.db"
+
+# functions
+# print all games
+
+# print all team members
+
+# print blind round games
+
+# print all choice round games
+
+# find a student id
+
+# sort all names by alphabetical order
+
+# sort all blind round games by difficulty
+
+# sort all choice round games by difficulty
+
+# sort all games by popularity
+
+# print games that can be played with less than 4 players
+
+# print games that can be played with 4 or more players
