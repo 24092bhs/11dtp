4ef121d78f0e11fac0cd99db623e0444170abd2b
24092bhs <24092@burnside.school.nz>
2026-04-23 08:50:47 +1200

    beginning of next lesson - finished 2nd function and added 3rd one that prints all blind round games

--- theatresports database folder/theatresports_code.py
+++ theatresports database folder/theatresports_code.py
@@ -16,7 +16,7 @@ def print_all_games():
     cursor.execute(print_games)
     results = cursor.fetchall()
     # loop through all results
-    print("Name:                                   Round:     Difficulty: Popularity: Min. of People:")
+    print("Name:                                   Round:     Difficulty: Popularity: Min. People:")
     for game in results:
         print(f"{game[1]:<40}{game[2]:<12}{game[3]:<12}{game[4]:<12}{game[5]:<12}")
     # end of loop
@@ -24,20 +24,33 @@ def print_all_games():
 
 # print all team members
 def print_all_members():
-    '''print all games nicely'''
+    '''print all members nicely'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     print_members = "SELECT * FROM team_member;"
-    cursor.execute(print_games)
+    cursor.execute(print_members)
     results = cursor.fetchall()
     # loop through all results
-    print("Name:                                   Round:     Difficulty: Popularity: Min. of People:")
-    for game in results:
-        print(f"{game[1]:<40}{game[2]:<12}{game[3]:<12}{game[4]:<12}{game[5]:<12}")
+    print("First Name: Last Name:")
+    for member in results:
+        print(f"{member[1]:<12}{member[2]:<12}")
     # end of loop
     db.close()
 
 # print blind round games
+def print_blind_round():
+    '''print all blind round games games nicely'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    print_blind = "SELECT * FROM theatre_game WHERE round = 'Blind';"
+    cursor.execute(print_blind)
+    results = cursor.fetchall()
+    # loop through all results
+    print("Name:                                   Round:     Difficulty: Popularity: Min. People:")
+    for game in results:
+        print(f"{game[1]:<40}{game[2]:<12}{game[3]:<12}{game[4]:<12}{game[5]:<12}")
+    # end of loop
+    db.close()
 
 # print all choice round games
 
@@ -55,3 +68,4 @@ def print_all_members():
 
 # print games that can be played with 4 or more players
 
+print_blind_round()
