2b51b5889f8812ca3305adc8da946068865a8a50
24092bhs <24092@burnside.school.nz>
2026-04-30 12:36:16 +1200

    worked on the find_id function to try and allow people to access the information of all members

--- theatresports database folder/theatresports_code.py
+++ theatresports database folder/theatresports_code.py
@@ -151,10 +151,40 @@ def members_by_last_name():
 
 # find a student id (NEEDS TO BE FINISHED)
 def find_id():
-    '''find the student id of one of the team members''' # needs to be worked on!
+    '''find the student id of one of the team members'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
-    find_student_id = "SELECT * FROM team_member WHERE first_name = 'Ashley';"
+    while True:
+        try:
+            member_name = int(input('''What student's information do you need?
+                                1. Ashley
+                                2. Charlotte
+                                3. Eileen
+                                4. Fiona
+                                5. Hannah
+                                6. Isabelle\n'''))
+            if member_name == number_one:
+                find_student_id = "SELECT * FROM team_member WHERE first_name = 'Ashley';"
+                break
+            elif member_name == number_two:
+                find_student_id = "SELECT * FROM team_member WHERE first_name = 'Charlotte';"
+                break
+            elif member_name == number_three:
+                find_student_id = "SELECT * FROM team_member WHERE first_name = 'Eileen';"
+                break
+            elif member_name == number_four:
+                find_student_id = "SELECT * FROM team_member WHERE first_name = 'Fiona';"
+                break
+            elif member_name == number_five:
+                find_student_id = "SELECT * FROM team_member WHERE first_name = 'Hannah';"
+                break
+            elif member_name == number_six:
+                find_student_id = "SELECT * FROM team_member WHERE first_name = 'Isabelle';"
+                break
+            else:
+                print("That is not a member of the Theatresports team! Please enter a number from 1-6.")
+        except ValueError: 
+            print("Please enter a number from 1-6.")
     cursor.execute(find_student_id)
     results = cursor.fetchall()
     # loop through all results
@@ -168,7 +198,7 @@ def two_or_three_players():
     '''print all games nicely'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
-    less_than_four = "Changed to SELECT * FROM theatre_game WHERE min_people < 4 ORDER BY min_people ASC;"
+    less_than_four = "SELECT * FROM theatre_game WHERE min_people < 4 ORDER BY min_people ASC;"
     cursor.execute(less_than_four)
     results = cursor.fetchall()
     # loop through all results
@@ -236,11 +266,13 @@ while True:
             print_choice_round()
     # find games that can be played with certain numbers of players
     elif user_input == number_four:
-        number_of_players = int(input('How many people are in your team?'))
+        number_of_players = int(input('How many people are in your team?\n'))
         if number_of_players < number_four:
             two_or_three_players()
-        elif number_of_players >= number_four:
+        elif number_seven > number_of_players >= number_four:
             four_plus_players()
+        elif number_of_players >= number_seven:
+            print("Sorry, I cannot help you! You can only have up to six players per team.")
     # sorting members: if and elif statements for different was of sorting the data (need to include invalid inputs!)
     elif user_input == number_five:
         sorted_by = int(input('''\nDo you want it sorted by:
